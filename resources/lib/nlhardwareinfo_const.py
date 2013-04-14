f562b1838365dc90bba08f0caf8cbc537565cec6
Skipmode A1 <skipmodea1@outlook.com>
2013-04-14 10:32:39 +0200

    Commit

--- resources/lib/nlhardwareinfo_const.py
+++ resources/lib/nlhardwareinfo_const.py
@@ -9,4 +9,4 @@ __settings__    = xbmcaddon.Addon(id=__addon__ )
 __language__    = __settings__.getLocalizedString
 __images_path__ = os.path.join( xbmcaddon.Addon(id=__addon__ ).getAddonInfo('path'), 'resources', 'images' )
 __date__        = "10 april 2013"
-__version__     = "1.0.0"
+__version__     = "1.0.1"
