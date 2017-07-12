2b5b84f4908b8f22137594088ebd3b8d1c504705
skipmodea1 <skipmodea1@outlook.com>
2017-07-12 13:14:22 +0200

    v1.0.7 (2017-07-12)
    - added listitem.setInfo video for Kodi 18 and up
    - added sorting
    - added context item episode info

--- resources/lib/nlhardwareinfo_const.py
+++ resources/lib/nlhardwareinfo_const.py
@@ -8,8 +8,8 @@ import xbmcaddon
 # Constants
 # 
 ADDON = "plugin.video.nlhardwareinfo"
-SETTINGS = xbmcaddon.Addon(id=ADDON)
+SETTINGS = xbmcaddon.Addon()
 LANGUAGE = SETTINGS.getLocalizedString
-IMAGES_PATH = os.path.join(xbmcaddon.Addon(id=ADDON).getAddonInfo('path'), 'resources', 'images')
-DATE = "2017-03-12"
-VERSION = "1.0.6"
+IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'images')
+DATE = "2017-07-12"
+VERSION = "1.0.7"
